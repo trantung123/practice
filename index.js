b3c621c21b1adf00a3986da28e083201ba592017
TRAN THANH TUNG <tranthanhtung@TRANs-MacBook-Air.local>
2019-11-25 18:01:21 +0900

    improve cat method

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
+var Mouse = require('./Mouse');
 
-var dog = new Dog('PLUTO');
-var tom = new Cat();
+var cat = new Cat();
+var mouse = new Mouse('mickey');
+var dog = new Dog();
+// cat.eat(mouse);
+try {
+    cat.eat(dog);
+} catch(error) {
+    console.log('Error while cat eating a dog')
+}
 
-dog.eat(tom);
-console.log(dog);
+console.log(cat);
+// var dog = new Dog('PLUTO');
+// var tom = new Cat();
+
+// dog.eat(tom);
+// console.log(dog);
